d9eeac1f6f10f30b7b8af185b400af4cf918abb3
stepniakmikolaj <34920771+stepniakmikolaj@users.noreply.github.com>
2018-01-16 10:05:42 +0100

    Update Calculator.java

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,13 +1,11 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    int a;
-    int b;
 
-    //public Calculator(int a, int b) {
-    //    this.a = a;
-    //    this.b = b;
-    //}
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
 
     public int add(int a, int b) {
         return a + b;
