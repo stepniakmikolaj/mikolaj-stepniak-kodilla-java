4d7e249a5105fd52a5eb98f98af2fe260dc9fe74
stepniakmikolaj <stepniakmikolaj@gmail.com>
2018-01-24 22:02:59 +0100

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -3,6 +3,7 @@ package com.kodilla.stream.array;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
+
     static double getAverage(int[] numbers) {
         IntStream.range(0, numbers.length)
                 .map(i -> numbers[i])
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,7 +8,7 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage(){
         //Given
-        int[] n = {1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1};
+        int[] n = {1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1};
         //When
         double average = ArrayOperations.getAverage(n);
         double expected = 4.764;
