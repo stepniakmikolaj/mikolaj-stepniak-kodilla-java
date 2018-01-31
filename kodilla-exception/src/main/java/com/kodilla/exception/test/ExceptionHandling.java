a20ce095225b204c4cee05975e8abc843bd96082
stepniakmikolaj <stepniakmikolaj@gmail.com>
2018-01-31 12:28:35 +0100

    8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -7,12 +7,12 @@ public class ExceptionHandling {
         exceptionHandling.handleAnException();
     }
 
-    public void handleAnException(){
+    public void handleAnException() {
         SecondChallenge secondChallenge = new SecondChallenge();
         try {
             System.out.println(secondChallenge.probablyIWillThrowException(1,1.5));
         } catch (Exception e) {
-            System.out.println("(x >= 2 || x < 1 || y == 1.5)\nChange double x or double y" + e);
+            System.out.println("X or Y are not correct\n" + e);
         } finally {
             System.out.println("I'm finally. I'm always");
         }
