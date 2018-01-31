82b83b0e9bc4184a19c65b270730c2221d6d0ac1
stepniakmikolaj <stepniakmikolaj@gmail.com>
2018-01-31 17:46:26 +0100

    9.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,45 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+    public static void main(String[] args){
+
+        MovieStore movieStore = new MovieStore();
+        List<String> movieList = movieStore.getMovies().entrySet().stream()
+                .flatMap(a -> a.getValue().stream())
+                .collect(Collectors.toList());
+
+        StringJoiner stringJoiner = new StringJoiner(" ! ");
+        for(String e: movieList){
+            stringJoiner.add(e);
+        }
+
+        System.out.println(stringJoiner.toString());
+
+    }
+}
