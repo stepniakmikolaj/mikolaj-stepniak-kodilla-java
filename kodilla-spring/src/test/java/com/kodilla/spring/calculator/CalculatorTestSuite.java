3d6d5dd3512dd13b02f742b9d12a54289e47a32e
stepniakmikolaj <stepniakmikolaj@gmail.com>
2018-02-07 14:20:23 +0100

    10.2

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,60 @@
+package com.kodilla.spring.calculator;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testAdd() {
+        //G
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //W
+        double result = calculator.add(10.0, 5.0);
+        //T
+        assertEquals(15.0, result, 0.01);
+    }
+
+    @Test
+    public void testSub() {
+        //G
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //W
+        double result = calculator.sub(10.0, 5.0);
+        //T
+        assertEquals(5.0, result, 0.01);
+    }
+
+    @Test
+    public void testMul() {
+        //G
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //W
+        double result = calculator.mul(10.0, 5.0);
+        //T
+        assertEquals(50.0, result, 0.01);
+    }
+
+    @Test
+    public void testDiv() {
+        //G
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //W
+        double result = calculator.div(10.0, 5.0);
+        //T
+        assertEquals(2.0, result, 0.01);
+    }
+}
